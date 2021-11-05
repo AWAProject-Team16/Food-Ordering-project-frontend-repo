ce780527bc54dd44f67b3cdb0ae129ce8413b311
Hiachi <nth937@gmail.com>
2021-11-06 01:39:41 +0200

    Set up the frontend-repo with react-generator, Split folder

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import styles from './../css/Home.module.css'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
---
+++ src/components/Restaurant.js
@@ -0,0 +1,10 @@
+import styles from './../css/Restaurant.module.css'
+import React from 'react'
+
+export default function Restaurant() {
+    return (
+        <div>
+            Restaurant
+        </div>
+    )
+}
