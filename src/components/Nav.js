67553b9d2681feca7e6a8e6d60a3151f43441cd8
Kalle <kalleilmari@icloud.com>
2021-11-11 04:30:39 +0200

    Had to commit unsaved before pulling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,35 @@
 import styles from './../css/Nav.module.css'
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Link, NavLink } from "react-router-dom";
+import Modal from 'react-modal'
 
 export default class Nav extends Component {
-  render() {
+  
+ constructor(props) {
+    super(props);
+
+    this.state = { sign:false, login: false}
+    
+  }
+  onOpenRegister = () => {
+    this.setState({sign: true});
+  };
+
+  onOpenLogin = () => {
+    this.setState({login:true})
+  }
+
+  onCloseRegister = () => {
+    this.setState({sign:false})
+  }
+
+  onCloseLogin = () => {
+    this.setState({login:false})
+  }
+ 
+
+  render() {  
+    const { login, sign } = this.state;
     return (
       <div className={ styles.nav }>
         <ul>
@@ -12,8 +38,14 @@ export default class Nav extends Component {
        </li>
        </ul>
         <input className={styles.searchbar}type="text" placeholder="Search.."></input>
-        <button className={styles.button}>Log in</button>
-        <button className={styles.button2}>Register</button>
+        
+        <button className={styles.button} handleClick={ this.onOpenLogin}>Log in</button>
+        
+        <button className={styles.button2} onClick={ this.onOpenRegister}>Register</button>
+        
+        <Modal isOpen={sign} >
+          
+        </Modal>
         <ul>
           <li>
             <Link to="/restaurant">Restaurant</Link>
