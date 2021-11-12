d90c95a995aef85c90b3f9083008448382508fc7
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2021-11-12 15:00:20 +0200

    Fix typo register, create new restaurant (#15, #8)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import RouterURL from './router/RouterURL';
-import RestaurantCreateNew from './components/RestaurantCreateNew';
-import Register from './components/Register';
 
 import styles from './App.module.css'
 import React, { Component } from 'react'
@@ -11,7 +9,6 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <RestaurantCreateNew />
         <div className= { styles.App }>
           <Nav />
           <RouterURL />
--- src/router/RouterURL.js
+++ src/router/RouterURL.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, {Component} from 'react';
+import {Routes, Route} from "react-router-dom";
 import Home from '../components/Home';
 import Restaurant from '../components/Restaurant';
+import RestaurantCreateNew from '../components/RestaurantCreateNew';
+import Register from '../components/Register';
 import ShoppingCart from '../components/ShoppingCart';
 
 export default class RouterURL extends Component {
@@ -11,6 +13,7 @@ export default class RouterURL extends Component {
         <Route path="/" element={<Home />} />
         <Route path="/restaurant" element={<Restaurant />} />
         <Route path="/shoppingcart" element={<ShoppingCart />} />
+        <Route path="/devthuc" element={<RestaurantCreateNew />} />
       </Routes>
     )
   }
