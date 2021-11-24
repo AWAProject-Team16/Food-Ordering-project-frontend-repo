e3e23f383cba7104c9cfd20848e1c4fdd4b4dc74
Hiachi <nth937@gmail.com>
2021-11-24 11:20:58 +0200

    #54 Task 1 - In function component: Get restaurants/:id/ page & product page via API

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../css/Restaurant.module.css';
 import { useParams } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 import RestaurantDetail from './RestaurantDetail';
+import axios from 'axios';
 
 export default function Restaurant(props) {
   // const result = window.location.pathname.split('/')[2]
   const result = useParams()
-  const obj = props.restaurants.find(item => item.idrestaurants === parseInt(result.idOfRestaurant));
-  if(obj == null) {
-    return <div>No matching restaurant</div>
+
+  const [obj, setObj] = useState({})
+  useEffect(() => {
+    axios.get(`http://localhost:5000/restaurants/id/${result.idOfRestaurant}`)
+      .then((res) => {
+        setObj(res.data[0])
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  // delete props from routerURL sau !!!
+  // const obj = props.restaurants.find(item => item.idrestaurants === parseInt(result.idOfRestaurant));
+  // if(obj == null) {
+  if(obj.idrestaurants == null) {
+    console.log('hehe')  // sao lại phải render 2 lần, dư thừa !
+    return <div><h1 style={{textAlign: 'center'}}>No matching restaurant</h1></div>
   }
   
   // console.log(props.categories)
   // const objCategories = props.categories.find(item => item.idrestaurants === obj.idrestaurants);
-  const objCategories = props.categories.filter(item => item.idrestaurants === obj.idrestaurants)
+  const objCategories = props.categories.filter(item => item.idrestaurants === obj.idrestaurants - 13)  // !!! tạm thời lấy jon nên có -13
   // console.log(objCategories)
   
-  const objProducts = props.products.filter(item => item.idrestaurants === obj.idrestaurants)
+  const objProducts = props.products.filter(item => item.idrestaurants === obj.idrestaurants - 13)
   
   return (
     <div>
